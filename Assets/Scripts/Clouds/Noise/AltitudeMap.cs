40a59b0d33b450c275c82ef0205708d3ce94851d
Louis Garczynski <louis.roc@gmail.com>
2020-03-23 23:56:38 +0100

    Add skeleton for an altitude transform layer

---
+++ Assets/Scripts/Clouds/Noise/AltitudeMap.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class AltitudeMap : MonoBehaviour {
+
+    public AltitudeMapSettings altitudeSettings;
+    public int resolution = 1024;
+    public RenderTexture altitudeMap;
+    public float altitudeOffset;
+    public float altitudeMultiplier;
+
+    public bool viewerEnabled;
+    [HideInInspector]
+    public bool showSettingsEditor = true;
+    bool shouldUpdateMap = true;
+    //public int[] minMaxTest;
+    double Remap (double v, double minOld, double maxOld, double minNew, double maxNew) {
+        return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
+    }
+
+    double GetAltitudeDensity (double heightPercent) {
+
+        var set = altitudeSettings;
+
+        double height = Remap (heightPercent, set.lowestLayerAltitude, set.highestLayerAltitude, -0.1, 1.2);
+        if (height > 1)
+            height = 2 - height;
+        else if (height < 0)
+            height = System.Math.Abs (height);
+
+        double result = System.Math.Abs (set.linearOffsetStart + height * set.linearOffsetFactor);
+        double multiplier = set.initialPowerFactor;
+        double originalHeight = height;
+
+        for (int i = 0; i < set.iterativePowerCount; i++) {
+            double invertedGradient = System.Math.Abs (Remap (height, 0, 1, -1.2, 1.2));
+            if (invertedGradient > 1)
+                invertedGradient = (2 - System.Math.Abs (invertedGradient));
+
+            result += System.Math.Pow (invertedGradient, 4) * multiplier;
+            multiplier *= set.iterativePowerFactor;
+            if (height > 0.5)
+                height = height - 0.5;
+            height *= 2;
+        }
+
+        return result
+            * System.Math.Pow (1 - originalHeight, set.powerOffsetPower) * set.powerOffsetRatio
+            + System.Math.Pow (originalHeight, set.powerOffsetPowerTop) * set.powerOffsetRatioTop;
+    }
+
+    public void UpdateMap () {
+
+        //if (altitudeSettings)
+
+        shouldUpdateMap = false;
+        ValidateParamaters();
+
+        CreateTexture1D (ref altitudeMap, resolution, "altitudeMap");
+        Texture2D temp = new Texture2D(resolution, 1);
+
+        double min = float.PositiveInfinity;
+        double max = float.NegativeInfinity;
+
+        double[] valueArray = new double[resolution];
+
+
+        for (int i = 0; i < resolution; i++) {
+            double value = GetAltitudeDensity(i / (float)(resolution - 1));
+            if (value < min) min = value;
+            if (value > max) max = value;
+            valueArray[i] = value;
+        }
+
+        for (int i = 0; i < resolution; i++) {
+            double value = valueArray[i];
+            value = (value - min) / (max - min);
+            temp.SetPixel (i, 0, new Color((float)value, (float)value, (float)value, 0));
+        }
+
+        altitudeOffset = (float)min;
+        altitudeMultiplier = (float)(max - min);
+
+        temp.Apply();
+
+        RenderTexture.active = altitudeMap;
+        Graphics.Blit(temp, altitudeMap);
+        RenderTexture.active = null;
+    }
+
+    //TODO move all CreateTexture to new TextureTools static file
+    void CreateTexture1D (ref RenderTexture texture, int resolution, string name) {
+        var format = GraphicsFormat.R16_UNorm;
+        if (texture == null || !texture.IsCreated () ||
+            texture.dimension != TextureDimension.Tex2D ||
+            texture.width != resolution ||
+            texture.height != 1 ||
+            texture.graphicsFormat != format) {
+            if (texture != null) {
+                texture.Release ();
+            }
+            texture = new RenderTexture (resolution, 1, 0);
+            texture.graphicsFormat = format;
+            texture.enableRandomWrite = true;
+            texture.dimension = UnityEngine.Rendering.TextureDimension.Tex2D;
+            texture.name = name;
+
+            texture.Create ();
+        }
+        texture.wrapMode = TextureWrapMode.Mirror;
+        texture.filterMode = FilterMode.Bilinear;
+    }
+
+    void OnValidate() {
+        shouldUpdateMap = true;
+        ValidateParamaters();
+    }
+
+    void ValidateParamaters () {
+        resolution = Mathf.Max(resolution, 1);
+    }
+}
---
+++ Assets/Scripts/Clouds/Noise/AltitudeMapSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AltitudeMapSettings : ScriptableObject {
+
+    [Range(0.5f,1.5f)]
+    public float highestLayerAltitude = 0.9f;
+    [Range(-0.5f, 0.5f)]
+    public float lowestLayerAltitude = 0.1f;
+
+    [Range(-10f, 10f)]
+    public float linearOffsetStart = 0.2f;
+    [Range(-10f, 10f)]
+    public float linearOffsetFactor = -1f;
+
+    [Range(0f, 10f)]
+    public float powerOffsetPower = 0.5f;
+    [Range(0f, 1f)]
+    public float powerOffsetRatio = 0.75f;
+
+    [Range(0f, 10f)]
+    public float powerOffsetPowerTop = 0.5f;
+    [Range(0f, 10f)]
+    public float powerOffsetRatioTop = 0;
+
+    [Range(0f, 10f)]
+    public float initialPowerFactor = 2f;
+    [Range(0f, 1f)]
+    public float iterativePowerFactor = 0.8f;
+    [Range(0, 10)]
+    public int iterativePowerCount = 3;
+
+}
