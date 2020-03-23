89ee1c95b1923fc270a8b6c5c4a9ad6931fa4dc8
Louis Garczynski <louis.roc@gmail.com>
2020-03-23 23:55:04 +0100

    Move the cloud layer with the player

---
+++ Assets/CloudFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudFollow : MonoBehaviour
+{
+    public Transform player;
+    void LateUpdate()
+    {
+        if (player)
+        {
+            Vector3 playerPos = player.position;
+            playerPos.y = 0;
+            transform.position = playerPos;
+        }
+    }
+}
