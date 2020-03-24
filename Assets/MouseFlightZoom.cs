298183c62c5b7dde9536250a7f17aaf3a9f9ad75
Louis Garczynski <louis.roc@gmail.com>
2020-03-24 05:33:15 +0100

    Add zoom to mousflight

---
+++ Assets/MouseFlightZoom.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MFlight.MouseFlightController))]
+public class MouseFlightZoom : MonoBehaviour
+{
+    public float scrollPower = 1f;
+
+    float _padScroll;
+    private void Update() {
+        if (MouseScroll != 0)
+        {
+            Debug.Log(MouseScroll);
+            GetComponent<MFlight.MouseFlightController>().offset *= 1 - MouseScroll * scrollPower ;
+        }
+    }
+
+    float MouseScroll
+    {
+        get
+        {
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+            if (mouseScroll != 0)
+                return mouseScroll;
+            else
+                return _padScroll;
+        }
+    }
+
+    //Get TrackPad Scroll
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.ScrollWheel)
+            _padScroll = -Event.current.delta.y / 100;
+        else
+            _padScroll = 0;
+    }
+}
