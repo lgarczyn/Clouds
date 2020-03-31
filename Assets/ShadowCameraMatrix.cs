2985bf0d97c11f9105247d9b2ed302bedd72e1bd
Louis Garczynski <louis.roc@gmail.com>
2020-03-31 23:26:06 +0200

    Cleaned up ShadowCameraMatrix

--- Assets/ShadowCameraMatrix.cs
+++ Assets/ShadowCameraMatrix.cs
@@ -7,113 +7,118 @@ using UnityEngine;
 public class ShadowCameraMatrix : MonoBehaviour
 {
     public Transform directionalLight;
-    public Transform activeCamera;
-    public Vector3 offset;
     public bool active = true;
+    public bool debug = false;
     public bool useSun = true;
     public Vector3 alternativeAngles;
-    public Matrix4x4 matrix = Matrix4x4.identity;
+    Matrix4x4 matrix = Matrix4x4.identity;
 
 
-    private void Update() {
+    private void LateUpdate() {
 
         Camera shadowCamera = GetComponent<Camera>();
 
+        // Force the camera to calculate depth
         shadowCamera.depthTextureMode = DepthTextureMode.Depth;
-        shadowCamera.orthographic = true;
 
         if (active)
         {
+            // Either use the sun or the input as our shadow direction
             Quaternion target;
-            if (useSun)
+            if (useSun && directionalLight != null)
                 target = directionalLight.rotation;
             else
                 target = Quaternion.Euler(alternativeAngles);
+
+            // Set the initial camera matrix
+            // Doesn't set the scaling, as Unity adds it from the camera parameters
             Matrix4x4 shadowMatrix = Matrix4x4.Ortho(-1, 1, -1, 1, -1, 1);
 
-            Vector3 angles = (Quaternion.Euler(-90,0,0) * directionalLight.rotation).eulerAngles;
+            // Get the angles of sunlight
+            Vector3 angles = (target).eulerAngles;
 
+            // Add shear to the custom matrix to match the sunlight
             matrix.m02 = Mathf.Tan(Mathf.Deg2Rad * angles.y);
             matrix.m12 = Mathf.Tan(Mathf.Deg2Rad * -angles.x);
 
-            // shadowMatrix =  Matrix4x4.TRS(transform.localPosition, Quaternion.identity, new Vector3(1,1,1));
+            // Combines the custom matrix, the ortho matrix, the current rotation, the current position
             shadowMatrix =
                 matrix
                 * shadowMatrix
-                * Matrix4x4.Rotate(target)
-                * Matrix4x4.Inverse(Matrix4x4.Rotate(transform.rotation))
+                * Matrix4x4.Rotate(Quaternion.Inverse(transform.rotation))
                 * Matrix4x4.Translate(-transform.position);
 
-            // Plane p = new Plane(Vector3.up, 1000);
-
-            //shadowCamera.worldToCameraMatrix = shadowMatrix;
-
-            //shadowMatrix = shadowCamera.CalculateObliqueMatrix(new Vector4(0,-1,0,0));// * shadowMatrix;
-
+            // Apply the matrix
             shadowCamera.worldToCameraMatrix = shadowMatrix;
         }
         else
             shadowCamera.ResetWorldToCameraMatrix();
 
+        if (debug == false)
+            return;
+
         // z is left positive for later signing
         // Vector4 is needed for translations to work
-        Vector4[] FrustumCornersNear = new Vector4[]{
+        // Near 0-3
+        // Far 4-7
+        Vector4[] frustumCorners = new Vector4[]{
             new Vector4(1, 1, 1, 1),
             new Vector4(1, -1, 1, 1),
             new Vector4(-1, 1, 1, 1),
-            new Vector4(-1, -1, 1, 1)};
-
-        Vector4[] FrustumCornersFar = new Vector4[]{
+            new Vector4(-1, -1, 1, 1),
             new Vector4(1, 1, 1, 1),
             new Vector4(1, -1, 1, 1),
             new Vector4(-1, 1, 1, 1),
             new Vector4(-1, -1, 1, 1)
         };
-        Vector4[] NormCorners = new Vector4[8];
 
-        // shadowCamera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), shadowCamera.nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, FrustumCornersNear);
-        // shadowCamera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), shadowCamera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, FrustumCornersFar);
+        // Because the custom camera projection matrix doesn't do either scaling or projection itself
+        // Only position rotation and shear
+        // We do the inverse projection and scaling ourselves to display the result
 
-        FrustumCornersNear.CopyTo(NormCorners, 0);
-        FrustumCornersFar.CopyTo(NormCorners, 4);
-
+        float fovMultiplier = Mathf.Tan(shadowCamera.fieldOfView * Mathf.Deg2Rad / 2);
         for (int i = 0; i < 8; i++)
         {
-            NormCorners[i].x *= shadowCamera.orthographicSize;
-            NormCorners[i].y *= shadowCamera.orthographicSize;
-            //dirty hack
             if (i < 4)
-                NormCorners[i].z *= shadowCamera.nearClipPlane;
+                frustumCorners[i].z *= shadowCamera.nearClipPlane;
             else
-                NormCorners[i].z *= shadowCamera.farClipPlane;
+                frustumCorners[i].z *= shadowCamera.farClipPlane;
 
-            // Z inversion because OpenGL or something
-            NormCorners[i].z *= -1;
+            float multiplier;
+            if (shadowCamera.orthographic)
+                multiplier = shadowCamera.orthographicSize;
+            else
+                multiplier = frustumCorners[i].z * fovMultiplier;
 
-            NormCorners[i] = shadowCamera.cameraToWorldMatrix /* transform.worldToLocalMatrix*/ * NormCorners[i];
-            //NormCorners[i] = shadowCamera.transform.TransformVector(NormCorners[i]);
-            //NormCorners[i] = NormCorners[i].normalized;
-            //Debug.Log(NormCorners[i]);
-            //Debug.Log(transform.TransformPoint(NormCorners[i]));
+            frustumCorners[i].x *= multiplier;
+            frustumCorners[i].y *= multiplier;
+
+            // Z inversion because OpenGL norm
+            frustumCorners[i].z *= -1;
+
+            frustumCorners[i] = shadowCamera.cameraToWorldMatrix * frustumCorners[i];
         }
-        Debug.DrawLine(NormCorners[0], NormCorners[4], Color.red);   //BL
-        Debug.DrawLine(NormCorners[1], NormCorners[5], Color.blue);  //TL
-        Debug.DrawLine(NormCorners[2], NormCorners[6], Color.yellow);//TR
-        Debug.DrawLine(NormCorners[3], NormCorners[7], Color.green); //BR
 
-        Debug.DrawLine(NormCorners[0], NormCorners[1], Color.black);   //BL
-        Debug.DrawLine(NormCorners[1], NormCorners[3], Color.black);  //TL
-        Debug.DrawLine(NormCorners[2], NormCorners[0], Color.black);//TR
-        Debug.DrawLine(NormCorners[3], NormCorners[2], Color.black); //BR
+        // Frustum
 
-        Debug.DrawLine(NormCorners[4], NormCorners[5], Color.white);   //BL
-        Debug.DrawLine(NormCorners[5], NormCorners[7], Color.white);  //TL
-        Debug.DrawLine(NormCorners[6], NormCorners[4], Color.white);//TR
-        Debug.DrawLine(NormCorners[7], NormCorners[6], Color.white); //BR
-        // MyMaterial.SetVector("_BL", NormCorners[0]);
-        // MyMaterial.SetVector("_TL", NormCorners[1]);
-        // MyMaterial.SetVector("_TR", NormCorners[2]);
-        // MyMaterial.SetVector("_BR", NormCorners[3]);
+        Debug.DrawLine(frustumCorners[0], frustumCorners[4], Color.red);
+        Debug.DrawLine(frustumCorners[1], frustumCorners[5], Color.blue);
+        Debug.DrawLine(frustumCorners[2], frustumCorners[6], Color.yellow);
+        Debug.DrawLine(frustumCorners[3], frustumCorners[7], Color.green);
+
+        // Near culling plane
+
+        Debug.DrawLine(frustumCorners[0], frustumCorners[1], Color.black);
+        Debug.DrawLine(frustumCorners[1], frustumCorners[3], Color.black);
+        Debug.DrawLine(frustumCorners[2], frustumCorners[0], Color.black);
+        Debug.DrawLine(frustumCorners[3], frustumCorners[2], Color.black);
+
+        // Far culling plane
+
+        Debug.DrawLine(frustumCorners[4], frustumCorners[5], Color.white);
+        Debug.DrawLine(frustumCorners[5], frustumCorners[7], Color.white);
+        Debug.DrawLine(frustumCorners[6], frustumCorners[4], Color.white);
+        Debug.DrawLine(frustumCorners[7], frustumCorners[6], Color.white);
     }
 
 }
