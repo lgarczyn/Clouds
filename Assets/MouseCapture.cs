0c59c3ee6a9cf5a80894ce2877232ce7e631aa79
Louis Garczynski <louis.roc@gmail.com>
2020-04-26 20:15:16 +0200

    Added mouse capture on Linux to handle editor issues

---
+++ Assets/MouseCapture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCapture : MonoBehaviour
+{
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.LinuxEditor || Application.isEditor == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+                Cursor.lockState = CursorLockMode.Locked;
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
