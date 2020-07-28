47166bf3e27c2df72a37d44e053d9ced159feb01
Louis Garczynski <l.garczynski@lylo.tv>
2020-07-28 23:07:18 +0200

    Added utility scripts

---
+++ Assets/EmissionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionController : MonoBehaviour
+{
+    public AnimationCurve intensityCurve;
+    public float intensity = 300;
+
+    public void SetValue(float percent)
+    {
+        float clampedIndex = Mathf.Clamp01(percent / 100);
+
+        float value = intensityCurve.Evaluate(clampedIndex);
+
+        float scaledValue = Mathf.Max(value, 0) * intensity;
+
+        var emission = GetComponent<ParticleSystem>().emission;
+
+        emission.rateOverTime = scaledValue;
+    }
+}
---
+++ Assets/Scripts/UI/ResourceBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBar : MonoBehaviour
+{
+    public void SetValue(float value)
+    {
+        float ratio = Mathf.Clamp01(value / 100);
+        GetComponent<RectTransform>().localScale = new Vector2(ratio, 1f);
+    }
+}
