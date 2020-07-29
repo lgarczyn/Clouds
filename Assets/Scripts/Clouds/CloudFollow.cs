8cf2bb5f56e57104a8203f484191d9ab3c91426e
Louis Garczynski <l.garczynski@lylo.tv>
2020-07-29 22:13:16 +0200

    Made horizon be further

--- Assets/Scripts/Clouds/CloudFollow.cs
+++ Assets/Scripts/Clouds/CloudFollow.cs
@@ -10,9 +10,9 @@ public class CloudFollow : MonoBehaviour
     void Start() {
         if (increaseOnPlay) {
             transform.localScale = new Vector3(
-                transform.localScale.x * 10,
+                transform.localScale.x * 100,
                 transform.localScale.y,
-                transform.localScale.z * 10
+                transform.localScale.z * 100
             );
         }
     }
