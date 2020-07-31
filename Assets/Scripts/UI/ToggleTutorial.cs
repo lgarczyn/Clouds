620b9f920c2ead86f801ca302990acff47747fa5
Louis Garczynski <l.garczynski@lylo.tv>
2020-07-31 02:44:34 +0200

    Added tuto toggle

---
+++ Assets/Scripts/UI/ToggleTutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTutorial : MonoBehaviour
+{
+    public KeyCode key;
+    public GameObject target;
+
+    void Update() {
+        if (Input.GetKeyDown(key))
+            target.SetActive(!target.activeSelf);
+    }
+}
