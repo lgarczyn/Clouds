b6cde17ad8c7432a33f9eee0c96cf2a048c02482
Louis Garczynski <louis.roc@gmail.com>
2020-08-17 02:13:15 +0200

    Removed warning

--- Assets/Scripts/Clouds/SetCameraDepthTextureMode.cs
+++ Assets/Scripts/Clouds/SetCameraDepthTextureMode.cs
@@ -7,12 +7,11 @@ using UnityEngine;
 public class SetCameraDepthTextureMode : MonoBehaviour {
 
     [SerializeField] private DepthTextureMode depthTextureMode = DepthTextureMode.Depth;
-    [SerializeField] private bool affectEditor = true;
 
     #if UNITY_EDITOR
     private void Update() {
         // Makes sure the editor view is also set to depth
-        if (affectEditor && Camera.current)
+        if (Camera.current)
         {
             Camera.current.depthTextureMode |= depthTextureMode;
         }
