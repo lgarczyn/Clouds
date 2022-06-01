0a980c1a63bf6053f7c05a972d897e2e36209d4c
Louis <louis.roc@gmail.com>
2022-06-02 00:20:01 +0200

    Reformatted scripts about to be changed

--- Assets/Scripts/Clouds/CloudFollow.cs
+++ Assets/Scripts/Clouds/CloudFollow.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class CloudFollow : MonoBehaviour
 {
-    public Transform player;
-    public bool increaseOnPlay = true;
+  public Transform player;
+  public bool increaseOnPlay = true;
 
-    void Start() {
-        if (increaseOnPlay) {
-            transform.localScale = new Vector3(
-                transform.localScale.x * 100,
-                transform.localScale.y,
-                transform.localScale.z * 100
-            );
-        }
-    }
-
-    void LateUpdate()
+  void Start()
+  {
+    if (increaseOnPlay)
     {
-        if (player)
-        {
-            Vector3 playerPos = player.position;
-            playerPos.y = 0;
-            transform.position = playerPos;
-        }
+      transform.localScale = new Vector3(
+          transform.localScale.x * 100,
+          transform.localScale.y,
+          transform.localScale.z * 100
+      );
     }
+  }
+
+  void LateUpdate()
+  {
+    if (player)
+    {
+      Vector3 playerPos = player.position;
+      playerPos.y = 0;
+      transform.position = playerPos;
+    }
+  }
 }
--- Assets/Scripts/Clouds/Noise/AltitudeMap.cs
+++ Assets/Scripts/Clouds/Noise/AltitudeMap.cs
@@ -4,118 +4,132 @@ using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 
-public class AltitudeMap : MonoBehaviour {
+public class AltitudeMap : MonoBehaviour
+{
 
-    public AltitudeMapSettings altitudeSettings;
+  public AltitudeMapSettings altitudeSettings;
 
-    [HideInInspector]
-    public bool showSettingsEditor = true;
+  [HideInInspector]
+  public bool showSettingsEditor = true;
 
-    public RenderTexture altitudeMap{
-        get {
-            UpdateMap();
-            return (_altitudeMap);
-        }
+  public RenderTexture altitudeMap
+  {
+    get
+    {
+      UpdateMap();
+      return (_altitudeMap);
     }
-    public float altitudeOffset {
-        get {
-            UpdateMap();
-            return (_altitudeOffset);
-        }
+  }
+  public float altitudeOffset
+  {
+    get
+    {
+      UpdateMap();
+      return (_altitudeOffset);
     }
-    public float altitudeMultiplier {
-        get {
-            UpdateMap();
-            return (_altitudeMultiplier);
-        }
+  }
+  public float altitudeMultiplier
+  {
+    get
+    {
+      UpdateMap();
+      return (_altitudeMultiplier);
     }
+  }
 
-    RenderTexture _altitudeMap;
-    float _altitudeOffset;
-    float _altitudeMultiplier;
+  RenderTexture _altitudeMap;
+  float _altitudeOffset;
+  float _altitudeMultiplier;
 
-    double Remap (double v, double minOld, double maxOld, double minNew, double maxNew) {
-        return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
-    }
+  double Remap(double v, double minOld, double maxOld, double minNew, double maxNew)
+  {
+    return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
+  }
 
-    double GetAltitudeDensity (double heightPercent) {
+  double GetAltitudeDensity(double heightPercent)
+  {
 
-        var set = altitudeSettings;
+    var set = altitudeSettings;
 
-        double height = Remap (heightPercent, set.lowestLayerAltitude, set.highestLayerAltitude, -0.1, 1.2);
-        if (height > 1)
-            height =  System.Math.Abs(2 - height);
-        else if (height < 0)
-            height = System.Math.Abs (height);
+    double height = Remap(heightPercent, set.lowestLayerAltitude, set.highestLayerAltitude, -0.1, 1.2);
+    if (height > 1)
+      height = System.Math.Abs(2 - height);
+    else if (height < 0)
+      height = System.Math.Abs(height);
 
-        double result = System.Math.Abs (set.linearOffsetStart + height * set.linearOffsetFactor);
-        double multiplier = set.initialPowerFactor;
-        double originalHeight = height;
+    double result = System.Math.Abs(set.linearOffsetStart + height * set.linearOffsetFactor);
+    double multiplier = set.initialPowerFactor;
+    double originalHeight = height;
 
-        for (int i = 0; i < set.iterativePowerCount; i++) {
-            double invertedGradient = System.Math.Abs (Remap (height, 0, 1, -1.2, 1.2));
-            if (invertedGradient > 1)
-                invertedGradient = (2 - System.Math.Abs (invertedGradient));
+    for (int i = 0; i < set.iterativePowerCount; i++)
+    {
+      double invertedGradient = System.Math.Abs(Remap(height, 0, 1, -1.2, 1.2));
+      if (invertedGradient > 1)
+        invertedGradient = (2 - System.Math.Abs(invertedGradient));
 
-            result += System.Math.Pow (invertedGradient, 4) * multiplier;
-            multiplier *= set.iterativePowerFactor;
-            if (height > 0.5)
-                height = height - 0.5;
-            height *= 2;
-        }
-
-        return result
-            * System.Math.Pow (1 - originalHeight, set.powerOffsetPower) * set.powerOffsetRatio
-            + System.Math.Pow (originalHeight, set.powerOffsetPowerTop) * set.powerOffsetRatioTop;
-    }
-
-    public void UpdateMap () {
-        if (_altitudeMap != null && altitudeSettings.isDirty == false)
-            return;
-
-        altitudeSettings.isDirty = false;
-
-        int resolution = altitudeSettings.resolution;
-
-        CreateTexture (ref _altitudeMap, resolution, "altitudeMap");
-        Texture2D temp = new Texture2D(resolution, 1);
-
-        double min = float.PositiveInfinity;
-        double max = float.NegativeInfinity;
-
-        double[] valueArray = new double[resolution];
-
-
-        for (int i = 0; i < resolution; i++) {
-            double value = GetAltitudeDensity(i / (float)(resolution - 1));
-            if (value < min) min = value;
-            if (value > max) max = value;
-            valueArray[i] = value;
-        }
-
-        for (int i = 0; i < resolution; i++) {
-            double value = valueArray[i];
-            value = (value - min) / (max - min);
-            temp.SetPixel (i, 0, new Color((float)value, (float)value, (float)value, 0));
-        }
-
-        _altitudeOffset = (float)min;
-        _altitudeMultiplier = (float)(max - min);
-
-        temp.Apply();
-
-        RenderTexture.active = _altitudeMap;
-        Graphics.Blit(temp, _altitudeMap);
-        RenderTexture.active = null;
+      result += System.Math.Pow(invertedGradient, 4) * multiplier;
+      multiplier *= set.iterativePowerFactor;
+      if (height > 0.5)
+        height = height - 0.5;
+      height *= 2;
     }
 
-    void CreateTexture (ref RenderTexture texture, int resolution, string name) {
+    return result
+        * System.Math.Pow(1 - originalHeight, set.powerOffsetPower) * set.powerOffsetRatio
+        + System.Math.Pow(originalHeight, set.powerOffsetPowerTop) * set.powerOffsetRatioTop;
+  }
 
-        RenderTextureDescriptor desc = TextureTools.GetDescriptorNoise1D_R(resolution);
+  public void UpdateMap()
+  {
+    if (_altitudeMap != null && altitudeSettings.isDirty == false)
+      return;
 
-        TextureTools.VerifyTexture(ref texture, desc, name);
+    altitudeSettings.isDirty = false;
 
-        texture.wrapMode = TextureWrapMode.Mirror;
-        texture.filterMode = FilterMode.Bilinear;
+    int resolution = altitudeSettings.resolution;
+
+    CreateTexture(ref _altitudeMap, resolution, "altitudeMap");
+    Texture2D temp = new Texture2D(resolution, 1);
+
+    double min = float.PositiveInfinity;
+    double max = float.NegativeInfinity;
+
+    double[] valueArray = new double[resolution];
+
+
+    for (int i = 0; i < resolution; i++)
+    {
+      double value = GetAltitudeDensity(i / (float)(resolution - 1));
+      if (value < min) min = value;
+      if (value > max) max = value;
+      valueArray[i] = value;
     }
+
+    for (int i = 0; i < resolution; i++)
+    {
+      double value = valueArray[i];
+      value = (value - min) / (max - min);
+      temp.SetPixel(i, 0, new Color((float)value, (float)value, (float)value, 0));
+    }
+
+    _altitudeOffset = (float)min;
+    _altitudeMultiplier = (float)(max - min);
+
+    temp.Apply();
+
+    RenderTexture.active = _altitudeMap;
+    Graphics.Blit(temp, _altitudeMap);
+    RenderTexture.active = null;
+  }
+
+  void CreateTexture(ref RenderTexture texture, int resolution, string name)
+  {
+
+    RenderTextureDescriptor desc = TextureTools.GetDescriptorNoise1D_R(resolution);
+
+    TextureTools.VerifyTexture(ref texture, desc, name);
+
+    texture.wrapMode = TextureWrapMode.Mirror;
+    texture.filterMode = FilterMode.Bilinear;
+  }
 }
