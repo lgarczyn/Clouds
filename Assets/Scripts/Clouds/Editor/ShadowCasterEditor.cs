4170a6df5b7f0346bb4fa5fe066325d8c5bf090f
Louis <louis.roc@gmail.com>
2022-07-13 03:33:53 +0200

    Added shadowmap visualization

---
+++ Assets/Scripts/Clouds/Editor/ShadowCasterEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ShadowCameraMatrix))]
+public class ShadowCameraMatrixEditor : UnityEditor.Editor
+{
+
+  ShadowCameraMatrix shadowCaster;
+
+  bool folded = true;
+
+  public override void OnInspectorGUI()
+  {
+    DrawDefaultInspector();
+
+    folded = EditorGUILayout.Foldout(folded, "Preview");
+
+    if (folded)
+    {
+      GUILayout.Label(shadowCaster.GetComponent<Camera>().activeTexture);
+    }
+  }
+
+  void OnEnable()
+  {
+    shadowCaster = (ShadowCameraMatrix)target;
+  }
+}
