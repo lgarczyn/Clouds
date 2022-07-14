4f186fd39727674da20368d2d0f6c5b6e9e33327
Louis <louis.roc@gmail.com>
2022-07-14 20:07:20 +0200

    Cleaned plane cheat controller

--- Assets/Scripts/Controllers/PlaneCheatController.cs
+++ Assets/Scripts/Controllers/PlaneCheatController.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MFlight.Demo.Plane))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlaneCheatController : MonoBehaviour
 {
   public float baseSpeed = 60;
-  public float shiftSpeedMultiplier = 10;
-  public float controlSpeedMultiplier = 100;
+  public float shiftSpeedMultiplier = 7;
+  public float controlSpeedMultiplier = 40;
 
   void Update()
   {
-    GetComponent<MFlight.Demo.Plane>().thrust = baseSpeed;
-    if (Input.GetKey(KeyCode.LeftShift)) GetComponent<MFlight.Demo.Plane>().thrust *= shiftSpeedMultiplier;
-    if (Input.GetKey(KeyCode.LeftControl)) GetComponent<MFlight.Demo.Plane>().thrust *= controlSpeedMultiplier;
-    if (Input.GetKeyDown(KeyCode.K)) GetComponent<Rigidbody>().position = Vector3.zero;
-    if (Input.GetKeyDown(KeyCode.K)) GetComponent<Rigidbody>().velocity = Vector3.zero;
-    if (Input.GetKeyDown(KeyCode.L)) GetComponent<Rigidbody>().isKinematic = !GetComponent<Rigidbody>().isKinematic;
+    MFlight.Demo.Plane plane = GetComponent<MFlight.Demo.Plane>();
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+    plane.thrust = baseSpeed;
+    if (Input.GetKey(KeyCode.LeftShift)) plane.thrust *= shiftSpeedMultiplier;
+    if (Input.GetKey(KeyCode.LeftControl)) plane.thrust *= controlSpeedMultiplier;
+    if (Input.GetKeyDown(KeyCode.L)) rigidbody.isKinematic = !rigidbody.isKinematic;
+    if (Input.GetKeyDown(KeyCode.K))
+    {
+      rigidbody.position = Vector3.zero;
+      if (!rigidbody.isKinematic) rigidbody.velocity = Vector3.zero;
+    }
   }
 }
