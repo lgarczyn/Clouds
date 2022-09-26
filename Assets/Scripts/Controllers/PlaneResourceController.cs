fd373c1e0efadd9194511e4df99f0dd488944d97
Louis <louis.roc@gmail.com>
2022-09-26 04:41:35 +0200

    Removed matter gameplay, and clouds recharge boost

--- Assets/Scripts/Controllers/PlaneResourceController.cs
+++ Assets/Scripts/Controllers/PlaneResourceController.cs
@@ -1,49 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Serialization;
 using UnityEngine;
 
-public abstract class ResourceCalculator: MonoBehaviour  {
-    public abstract float GetLight();
-    public abstract float GetDensity();
+public abstract class ResourceCalculator : MonoBehaviour
+{
+  public abstract float GetLight();
+  public abstract float GetDensity();
 }
 
 public class PlaneResourceController : MonoBehaviour
 {
-    public PlaneEntity plane;
-    public ResourceCalculator resourceCalculator;
+  public PlaneEntity plane;
+  public ResourceCalculator resourceCalculator;
 
-    public PlaneDeathController deathController;
-    public float energyMultiplier = 1f;
-    public float matterMultiplier = 1f;
+  public PlaneDeathController deathController;
+  public AnimationCurve energyVsDensity;
 
-    public float baseEnergyPerSecond = 1f;
+  void FixedUpdate()
+  {
+    float light = resourceCalculator.GetLight();
+    float density = resourceCalculator.GetDensity();
 
-    public AnimationCurve matterVsDensity;
-    public AnimationCurve matterVsDepth;
-    public AnimationCurve energyVsLight;
-
-
-    void FixedUpdate()
+    if (light < 0f)
     {
-        float light = resourceCalculator.GetLight();
-        float density = resourceCalculator.GetDensity();
-
-        if (light < 0f)
-        {
-
-            // TODO display error to client
-            // If not on startup and resources still cannot be retrieved
-            if (Time.time > 1) Debug.LogWarning("Resource controller cannot calculate resources");
-            return ;
-        }
-
-        float depth = Mathf.Clamp01(-plane.transform.position.y / 1400f + 0.5f); 
-
-        plane.RefuelEnergy(energyMultiplier * energyVsLight.Evaluate(light) * Time.fixedDeltaTime);
-        plane.RefuelMatter(matterMultiplier * matterVsDensity.Evaluate(density) * matterVsDepth.Evaluate(depth) * Time.fixedDeltaTime);
-
-        if (plane.TrySpendEnergy(baseEnergyPerSecond * Time.fixedDeltaTime) == false) {
-            deathController.KillPlane();
-        }
+      // TODO display error to client
+      // If not on startup and resources still cannot be retrieved
+      if (Time.time > 1) Debug.LogWarning("Resource controller cannot calculate resources");
+      return;
     }
+
+    plane.RefuelEnergy(energyVsDensity.Evaluate(density) * Time.fixedDeltaTime);
+  }
 }
