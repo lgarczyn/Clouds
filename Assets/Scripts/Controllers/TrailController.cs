65183e79fc9a2d5d718bf724b05b276615e26526
Louis <louis.roc@gmail.com>
2022-09-27 04:24:16 +0200

    Added missing trail controller

---
+++ Assets/Scripts/Controllers/TrailController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class TrailController : MonoBehaviour
+{
+  [PercentCurve(4)]
+  public AnimationCurve widthVsValue;
+  public float widthMultiplier = 1f;
+
+  public bool forceMax = false;
+
+  public TrailRenderer trail
+  {
+    get
+    {
+      return GetComponent<TrailRenderer>();
+    }
+  }
+
+  public void Start()
+  {
+    trail.widthMultiplier = 0f;//forceMax ? widthMultiplier : 0f;
+  }
+
+  public void SetValue(float percent)
+  {
+    float clampedPercent = Mathf.Clamp(percent, 0, 100);
+
+    float width = widthVsValue.Evaluate(clampedPercent) * widthMultiplier;
+
+    if (forceMax) width = widthMultiplier;
+
+    trail.widthMultiplier = width;
+  }
+
+  public void SetEmitting(bool value)
+  {
+    trail.emitting = value;
+  }
+
+  public void Clear()
+  {
+    trail.Clear();
+  }
+}
