1571ea8ac079e1ea003c801bb73d471643cd138a
Louis <louis.roc@gmail.com>
2022-11-16 17:04:49 +0100

    Moved to lambda form for player manager accessors

--- Assets/Scripts/Managers/PlayerManagerBridge.cs
+++ Assets/Scripts/Managers/PlayerManagerBridge.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerManagerBridge : ManagerBridge<PlayerManager>
 {
-  public Rigidbody playerRigidbody { get { return instance.playerRigidbody; } }
+  public Rigidbody playerRigidbody => instance.playerRigidbody;
 
-  public Transform playerTransform { get { return instance.playerTransform; } }
+  public Transform playerTransform => instance.playerTransform;
 
   public float Distance(Vector3 position) { return instance.Distance(position); }
 }
