bc933a4fac284ae515e9877990427627f15fb057
Louis <louis.roc@gmail.com>
2023-01-29 00:19:45 +0100

    Clarified comments

--- Assets/Scripts/Pools/PoolSubject.cs
+++ Assets/Scripts/Pools/PoolSubject.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.Events;
 
 /// <summary>
 /// Script allowing a pooled object to self-release
-/// Also allows the object to reset itself on release/init
+/// Also allows the prefab to reset itself on release/init
 /// </summary>
 public class PoolSubject : MonoBehaviour
 {
