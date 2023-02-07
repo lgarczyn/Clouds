9b71b29cd41d84ec9df37bfa950d2e87b9c1e90c
Lou Garczynski <lgarczyn@student.42.fr>
2023-02-07 16:09:21 +0100

    Updated pause system to be more resilient

--- Assets/Scripts/UI/MouseCapture.cs
+++ Assets/Scripts/UI/MouseCapture.cs
@@ -1,28 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class MouseCapture : MonoBehaviour
 {
-  bool isCaptured {
-    get => Cursor.lockState != CursorLockMode.None;
-    set {
-      Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
-      Cursor.visible = !value;
+  public UnityEvent<bool> onCaptureChange;
+
+  bool lastValue = false;
+
+  void Update()
+  {
+    CheckUpdated();
+  }
+
+  void CheckUpdated()
+  {
+    if (lastValue != IsCaptured)
+    {
+      lastValue = IsCaptured;
+      onCaptureChange.Invoke(IsCaptured);
     }
   }
 
-  [SerializeField] bool shouldCapture = true;
-
-  public bool ShouldCapture {
-    set { shouldCapture = true; isCaptured = true; }
-  }
-
-  public void OnClickIn() {
-    
-    if (shouldCapture) isCaptured = true;
-  }
-
-  public void OnEscape() {
-    isCaptured = false;
+  public bool IsCaptured
+  {
+    get => Cursor.lockState != CursorLockMode.None;
+    set
+    {
+      Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
+      Cursor.visible = !value;
+      CheckUpdated();
+    }
   }
 }
--- Assets/Scripts/Utils/TogglePlayMode.cs
+++ Assets/Scripts/Utils/TogglePlayMode.cs
@@ -1,43 +1,62 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
-using System.Linq;
 
 [RequireComponent(typeof(MouseCapture))]
+[RequireComponent(typeof(PlayerInput))]
 public class TogglePlayMode : MonoBehaviour
 {
   [SerializeField][RequiredComponent] MouseCapture reqMouseCapture;
+  [SerializeField][RequiredComponent] PlayerInput reqPlayerInput;
 
-  bool playing
+  bool lastPlayingState = true;
+
+  bool Playing
   {
     get => Time.timeScale != 0f;
     set
     {
       Time.timeScale = value ? 1f : 0f;
       AudioListener.pause = !value;
+
+      reqPlayerInput.SwitchCurrentActionMap(value ? "Game" : "UI");
+      reqMouseCapture.IsCaptured = value;
     }
   }
 
   // Check just in case scene was reloaded while paused
   void Start()
   {
-    playing = true;
-    reqMouseCapture.ShouldCapture = true;
+    Playing = true;
   }
 
   public void OnPlayPause(InputAction.CallbackContext context)
   {
     if (context.phase != InputActionPhase.Performed) return;
 
-    playing = !playing;
-    reqMouseCapture.ShouldCapture = playing;
+    Playing = !Playing;
   }
 
   public void OnReload(InputAction.CallbackContext context)
   {
     if (context.phase != InputActionPhase.Performed) return;
 
-    playing = true;
+    Playing = true;
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
+
+  private void OnApplicationFocus(bool focusStatus)
+  {
+    // If focusing in, restore play status
+    if (focusStatus)
+    {
+      Playing = lastPlayingState;
+    }
+    // If focusing out, store state and pause
+    else
+    {
+      lastPlayingState = Playing;
+      Playing = false;
+    }
   }
 }
