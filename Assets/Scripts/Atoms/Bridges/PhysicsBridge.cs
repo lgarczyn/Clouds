eb3ca8fb54948544c8026e619af32ff629a924dc
Lou Garczynski <lgarczyn@student.42.fr>
2023-02-11 23:00:41 +0100

    Added physics bridge to change physics using scaler

---
+++ Assets/Scripts/Atoms/Bridges/PhysicsBridge.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Atoms.Bridges
+{
+  [CreateAssetMenu(fileName = "Physics", menuName = "Atoms/Bridges/Physics", order = 0)]
+  public class PhysicsBridge : ScriptableObject
+  {
+    public void SetGravity(float gravity)
+    {
+      Physics.gravity = Vector3.down * gravity;
+    }
+  }
+}
