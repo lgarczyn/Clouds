dafacd33a5ac3010e2432668b04cbeeedb857b35
Lou Garczynski <lgarczyn@student.42.fr>
2023-02-11 22:58:07 +0100

    Remove todo

--- Assets/Scripts/Atoms/FloatMixer.cs
+++ Assets/Scripts/Atoms/FloatMixer.cs
@@ -8,86 +8,90 @@ using UnityEngine.Events;
 
 namespace Atoms
 {
-    [Serializable]
-    public enum MixingType
+  [Serializable]
+  public enum MixingType
+  {
+    Average,
+    Multiply,
+    Add,
+    Minimum,
+    Maximum
+  }
+
+  public class FloatMixer : MonoBehaviour
+  {
+    [SerializeField] MixingType mixing = MixingType.Multiply;
+    [SerializeField] List<FloatReference> inputs;
+    [SerializeField] UnityEvent<float> unityOutput;
+    [SerializeField] UnityEvent<Vector3> scaleOutput;
+    [SerializeField] FloatEvent atomOutput;
+
+    float _lastOutput = float.NaN;
+
+    List<FloatEvent> _listeners;
+
+    void OnEnable()
     {
-        Average,
-        Multiply,
-        Add,
-        Minimum,
-        Maximum,
+      AddListeners();
+      Raise();
     }
-    public class FloatMixer : MonoBehaviour
+
+    void OnDisable()
     {
-        // TODO: change to some kind of managed event list
-        [SerializeField] MixingType mixing = MixingType.Multiply;
-        [SerializeField] List<FloatReference> inputs;
-        [SerializeField] UnityEvent<float> unityOutput;
-        [SerializeField] UnityEvent<Vector3> scaleOutput;
-        [SerializeField] FloatEvent atomOutput;
-
-        float _lastOutput = float.NaN;
-
-        List<FloatEvent> _listeners;
-
-        void OnEnable()
-        {
-            AddListeners();
-            Raise();
-        }
-
-        void OnDisable()
-        {
-            RemoveListeners();
-        }
-
-        void OnValidate()
-        {
-            if (inputs.Any(r => r == null || r.IsUnassigned)) Debug.LogWarning("Unassigned input on FloatMixer", this);
-            if (!Application.isPlaying) return;
-            RemoveListeners();
-            AddListeners();
-            Raise();
-        }
-
-        void AddListeners()
-        {
-            _listeners = inputs
-                .Where(r => r.Usage != AtomReferenceUsage.VALUE)
-                .Where(r => r.Usage != AtomReferenceUsage.CONSTANT)
-                .Select(r => r.GetEvent<FloatEvent>())
-                .ToList();
-            _listeners.ForEach(e => e.Register(Raise));
-        }
-
-        void RemoveListeners()
-        {
-            _listeners?.ForEach(e => e.Unregister(Raise));
-            _listeners = null;
-        }
-
-        void Raise()
-        {
-            IEnumerable<float> it = inputs
-                .Where(r => !r.IsUnassigned)
-                .Select(i => i.Value);
-
-            float output = mixing switch
-            {
-                MixingType.Average => it.Average(),
-                MixingType.Multiply => it.Aggregate((a, b) => a * b),
-                MixingType.Add => it.Aggregate((a, b) => a + b),
-                MixingType.Minimum => it.Min(),
-                MixingType.Maximum => it.Max(),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-
-            if (_lastOutput.Equals(output)) return;
-            _lastOutput = output;
-            
-            if (atomOutput) atomOutput.Raise(output);
-            unityOutput.Invoke(output);
-            scaleOutput.Invoke(output * Vector3.one);
-        }
+      RemoveListeners();
     }
+
+    void OnValidate()
+    {
+      if (inputs.Any(r => r == null || r.IsUnassigned)) Debug.LogWarning("Unassigned input on FloatMixer", this);
+      if (!Application.isPlaying) return;
+      RemoveListeners();
+      AddListeners();
+      Raise();
+      for (int i = 0; i < scaleOutput.GetPersistentEventCount(); i++)
+        scaleOutput.SetPersistentListenerState(i, UnityEventCallState.RuntimeOnly);
+      for (int i = 0; i < unityOutput.GetPersistentEventCount(); i++)
+        unityOutput.SetPersistentListenerState(i, UnityEventCallState.RuntimeOnly);
+    }
+
+    void AddListeners()
+    {
+      _listeners = inputs
+        .Where(r => r.Usage != AtomReferenceUsage.VALUE)
+        .Where(r => r.Usage != AtomReferenceUsage.CONSTANT)
+        .Select(r => r.GetEvent<FloatEvent>())
+        .ToList();
+      _listeners.ForEach(e => e.Register(Raise));
+    }
+
+    void RemoveListeners()
+    {
+      _listeners?.ForEach(e => e.Unregister(Raise));
+      _listeners = null;
+    }
+
+    void Raise()
+    {
+      IEnumerable<float> it = inputs
+        .Where(r => !r.IsUnassigned)
+        .Select(i => i.Value);
+
+      float output = mixing switch
+      {
+        MixingType.Average => it.Average(),
+        MixingType.Multiply => it.Aggregate((a, b) => a * b),
+        MixingType.Add => it.Aggregate((a, b) => a + b),
+        MixingType.Minimum => it.Min(),
+        MixingType.Maximum => it.Max(),
+        _ => throw new ArgumentOutOfRangeException()
+      };
+      
+      if (_lastOutput.Equals(output)) return;
+      _lastOutput = output;
+
+      if (atomOutput) atomOutput.Raise(output);
+      unityOutput.Invoke(output);
+      scaleOutput.Invoke(output * Vector3.one);
+    }
+  }
 }
