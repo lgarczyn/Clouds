643200eb11b726c0ed0b9d4315d94eca18405df4
Lou Garczynski <lgarczyn@student.42.fr>
2023-02-21 20:53:55 +0100

    Added script to emit audio event on atom event

---
+++ Assets/Scripts/Sound/EventAudioEmitter.cs
@@ -0,0 +1,12 @@
+﻿using UnityAtoms;
+using UnityEngine;
+
+namespace Sound
+{
+  public class EventAudioEmitter : AudioEmitter
+  {
+    [SerializeField] AtomEventBase emitterEvent;
+    void OnEnable() => emitterEvent.Register(Play);
+    void OnDisable() => emitterEvent.Unregister(Play);
+  }
+}
